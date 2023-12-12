ad460d4f07a70a87ef660275baba1e84831bab1f
Nicolas <nicolasbogadouai@gmail.com>
2023-12-12 18:08:47 -0300

    update

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from flask_mysqldb import MySQL
 from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager, login_user, logout_user, login_required
@@ -74,7 +74,12 @@ def home():
     viajes = cur.fetchall()
     session['viajes'] = viajes
     cur.close()
-    return render_template('home.html', viajes=viajes)
+
+    informacion_viaje = session.get('informacion_viaje', None)
+    cliente_info = session.get('cliente', None)
+
+    return render_template('home.html', viajes=viajes, informacion_viaje=informacion_viaje, cliente_info=cliente_info)
+
 
 
 #método para ingresar nuevo cliente
@@ -103,23 +108,22 @@ def asignar():
         try:    
             cliente = request.form['telefono']
             cur = db.connection.cursor()      
-            cur.execute("SELECT `nombre_apellido`,`domicilio`, `telefono` FROM `clientes` WHERE `telefono`= %(cliente)s", {"cliente":cliente})
+            cur.execute("SELECT * FROM `clientes` WHERE `telefono`= %(cliente)s", {"cliente":cliente})
             cliente = cur.fetchall()
             session['cliente'] = cliente 
 
-
             cur.execute("SELECT * FROM `choferes` WHERE `estado` IS NULL OR `estado`=0")# filtro los choferes con el estado = 0, es decir sin viajes
             choferes = cur.fetchall()
-
-
             cur.close()
+
             if not cliente:
                 flash("No se ha encontrado el cliente...")
                 return render_template('home.html')
+            
             else:
                 return render_template('viajes.html', cliente=cliente, choferes=choferes)
         except Exception as e:
-            logger.error("Error en la conexión", str(e))
+            logger.error("Error en la conexión: %s", str(e))
             print(e)
             return redirect(url_for('home'))
     return render_template('viajes.html')
@@ -130,49 +134,106 @@ def asignar():
 def viajes():
     choferes = []
     cliente_id = None
-    cliente = None
+    telefono_cliente = None
+    fecha = None
+    direccion = None
+    destino = None
 
     if request.method == 'POST':
         try:
-            cliente = session.get('cliente')
-            cliente_id = cliente[0][0]
-            telefono_cliente = cliente[0][1]
-                     
-            choferes = request.form.getlist('choferes')
+            # Obtener la información del cliente de la sesión
+            cliente_info = session.get('cliente')
             
-            fecha = request.form['fecha']
-            origen = request.form['origen']
-            destino = request.form['destino']
-            estado = True    
+            if cliente_info:
+                cliente_id = cliente_info[0][0]
+                telefono_cliente = cliente_info[0][1]
+                session['cliente_id'] = cliente_id
+                session['telefono_cliente'] = telefono_cliente
+                
+                # Resto del código sigue igual
+                choferes = request.form.getlist('choferes')
+                fecha = request.form['fecha']
+                session['fecha'] = fecha
+                direccion = request.form['direccion']
+                session['direccion'] = direccion
+                destino = request.form['destino']
+                session['destino'] = destino
+                estado = True    
 
-            cur = db.connection.cursor()
-            
-            cur.execute("INSERT INTO `viajes` (cliente, telefono, chofer, fecha, origen, destino, estado) VALUES (%(cliente_id)s, %(telefono)s,%(chofer)s, %(fecha)s, %(origen)s, %(destino)s, %(estado)s)", {"cliente_id": cliente_id, "telefono": telefono_cliente,"chofer": choferes, "fecha": fecha, "origen": origen, "destino": destino, "estado": estado})
-            rowcount = cur.rowcount # chequeo si hubo una modificación (devulve números de filas afectadas)
-            cur.execute("UPDATE `choferes` SET `estado`=1 WHERE `id` IN %s", (tuple(choferes),))  # Usa IN para actualizar múltiples choferes
-            cur.connection.commit()
-            # Obtener el ID del viaje insertado
-            cur.execute("SELECT LAST_INSERT_ID()")  # Esto puede variar según la base de datos que estés utilizando
-            viaje_id = cur.fetchone()[0]
+                cur = db.connection.cursor()
+                
+                cur.execute("INSERT INTO `viajes` (cliente, telefono, chofer, fecha, direccion, destino, estado) VALUES (%(cliente_id)s, %(telefono)s, %(chofer)s, %(fecha)s, %(direccion)s, %(destino)s, %(estado)s)", {"cliente_id": cliente_id, "telefono": telefono_cliente, "chofer": tuple(choferes), "fecha": fecha, "direccion": direccion, "destino": destino, "estado": estado})
+                rowcount = cur.rowcount
+                cur.execute("UPDATE `choferes` SET `estado`=1 WHERE `id` IN %(chofer)s", {"chofer": tuple(choferes)})
+                cur.connection.commit()
+                cur.execute("SELECT LAST_INSERT_ID()")
+                viaje_id = cur.fetchone()[0]
 
-            # Almacenar la información del viaje en la sesión usando un identificador único
-            viaje_info = {'viaje_id': viaje_id, 'choferes': choferes}
-            session[f'viaje_info_{viaje_id}'] = viaje_info
+                viaje_info = {'viaje_id': viaje_id, 'choferes': choferes}
+                print(viaje_info)
+                session[f'viaje_info_{viaje_id}'] = viaje_info
 
-            cur.close()
-            if rowcount > 0:
-                flash("Viaje creado correctamente", "success")
-                return redirect(url_for('home'))
+                cur.close()
+                
+                if rowcount > 0:
+                    flash("Viaje creado correctamente", "success")
+                    return redirect(url_for('home'))
+                else:
+                    flash("Error en la creación del viaje")    
             else:
-                flash("Error en la creación del viaje")    
+                flash("Error: No se ha encontrado el cliente.")
+                return redirect(url_for('home'))
+
         except Exception as e:
             logger.error("Error en la conexión:%s", str(e))
             print(e)
-            
             return redirect(url_for('home'))
 
-    return render_template('viajes.html', cliente=cliente, choferes=choferes)
+    return render_template('viajes.html', cliente_info=cliente_info, choferes=choferes, telefono_cliente=telefono_cliente, fecha=fecha, direccion=direccion, destino=destino)
 
+
+
+@app.route('/viajes_espera', methods=['GET', 'POST'])
+def viajes_espera():
+    if request.method == 'POST':
+        cliente_info = request.form.get('cliente_info')
+
+        if cliente_info:
+            # Descomponer la cadena cliente_info en partes
+            cliente_info_parts = cliente_info.split(',')
+
+            # Extraer los valores correspondientes
+            cliente_id, cliente_nombre, telefono_cliente = cliente_info_parts
+
+            # Resto del procesamiento del formulario
+            fecha = request.form.get('fecha')
+            direccion = request.form.get('direccion')
+            destino = request.form.get('destino')
+
+            informacion_viaje = {
+                'cliente_id': cliente_id,
+                'cliente': cliente_nombre,
+                'telefono_cliente': telefono_cliente,
+                'fecha': fecha,
+                'direccion': direccion,
+                'destino': destino
+            }
+
+            session['informacion_viaje'] = informacion_viaje  # Guarda la información en la sesión
+            print(informacion_viaje)
+
+            return jsonify(result='success', informacion_viaje=informacion_viaje)
+
+        else:
+            flash('Error: No se proporcionó información del cliente')
+            return jsonify(result='error', message='No se proporcionó información del cliente')
+
+    else:
+        flash('Error: Método de solicitud no válido')
+        return jsonify(result='error', message='Método de solicitud no válido')
+
+    
+    
 
 
 #ruta finalizar_viaje
@@ -181,8 +242,12 @@ def finalizar_viaje():
     viaje_id = request.form.get('viaje_id')  # Obtén el viaje_id del formulario
     chofer_id = request.form.get('chofer_id')  # Obtén el chofer_id del formulario
 
+    print(f"Viaje ID: {viaje_id}")
+    print(f"Chofer ID: {chofer_id}")
+
     viaje_info = session.get(f'viaje_info_{viaje_id}', [])
-    
+    print(viaje_info)
+
     if not viaje_info:
         return redirect(url_for('home'))
     
@@ -213,8 +278,9 @@ def nuevo_cliente():
 
 
 # ruta clientes historial
-@app.route('/clientes')
+@app.route('/clientes', methods=['GET','POST'])
 def clientes():
+    
     if True:
         try:
             cur = db.connection.cursor()
@@ -224,6 +290,35 @@ def clientes():
             flash("Error en la conexión" + str(e))
             return render_template('home.html')
     return render_template('clientes.html', listado_clientes=listado_clientes)
+
+
+# Nueva ruta para historial de un cliente específico
+@app.route('/historial_cliente', methods=['GET','POST'])
+def historial_cliente():
+    cliente_id = request.args.get('cliente_id')
+    try:
+        cur = db.connection.cursor()
+        cur.execute("SELECT `nombre_apellido` FROM `clientes` WHERE `id` = %(cliente_id)s", {'cliente_id': cliente_id})
+        fullname_cliente = cur.fetchone()
+        
+        if fullname_cliente:
+            nombre_apellido = fullname_cliente[0]
+
+            # Obtener el historial de viajes del cliente
+            cur.execute("SELECT * FROM `viajes` WHERE `cliente` = %(nombre_apellido)s", {'nombre_apellido': nombre_apellido})
+            historial_cliente = cur.fetchall()
+            cur.close()
+
+        else:
+            flash("Cliente no encontrado")
+            return render_template('home.html')
+       
+    except Exception as e:
+        flash("Error en la conexión" + str(e))
+        return render_template('home.html')
+
+    return render_template('historial_cliente.html', historial_cliente=historial_cliente)
+
 
 
 # ruta para completar base de datos de choferes
